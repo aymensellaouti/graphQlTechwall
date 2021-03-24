77b82a86bb7ee7ee9accdff2ab60b917bd85e5d6
aymen2782 <aymen.sellaouti@gmail.com>
2021-03-24 08:47:45 +0100

    relations + variables

--- src/db/db.mjs
+++ src/db/db.mjs
@@ -5,12 +5,26 @@ export const db = {
       name: "Apprendre GraphQl Query",
       content: "Entamer la partie Query",
       status: "WAITING",
+      userId: 1
     },
     {
       id: 2,
       name: "Apprendre GraphQl Schema",
       content: "Entamer la partie Schema",
       status: "DONE",
+      userId: 2
     },
   ],
+  users: [
+    {
+      id: 1,
+      name: 'aymen',
+      email: 'aymen@gmail.com'
+    },
+    {
+      id: 2,
+      name: 'skander',
+      email: 'skander@gmail.com'
+    }
+  ]
 };
